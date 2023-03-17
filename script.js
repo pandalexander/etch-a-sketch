563ad9a13cd41d09dd2858c7361311fc87793920
Alex Seeley <alexseeley134@gmail.com>
2023-03-16 21:44:43 -0600

    Make number of divs responsive to any size
    I wrote my javascript in a way that will allow the ratio of squares
    inside the sketch box to be changed and sized to a particular
    width and height. This will allow freedom going forward to add or remove
    as many divs as I want.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // inside-div
 
 const sketchBox = document.getElementById("sketch-box");
-let numberOfDivs = 256;
+let numberOfSides = 16;
+let numberOfDivs = Math.pow(numberOfSides, 2);
 
 for (i = 0; i < numberOfDivs; i++) {
   let insideDiv = document.createElement("div");
   insideDiv.classList.add("inside-div");
+  insideDiv.style.width = 100 / numberOfSides + "%";
+  insideDiv.style.height = 100 / numberOfSides + "%";
+
   sketchBox.appendChild(insideDiv);
 }
